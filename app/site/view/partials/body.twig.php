f9e2bb16d64203d721e542e3240a7674da5512c6
edualvesz <ea-hendrix@hotmail.com>
2022-06-10 21:28:38 -0300

    nav bar was created

--- app/site/view/partials/body.twig.php
+++ app/site/view/partials/body.twig.php
@@ -5,14 +5,69 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="{{BASE}}img/favicon.ico">
+    <link rel="stylesheet" href="{{BASE}}css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{BASE}}css/style.css">
     <title>{% block title %} {% endblock %}</title>
 
     {% block head %} {% endblock %}
 
   </head>
-  <body>
-  
-  {% block body %} {% endblock %}
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="max-width">
+        <div class="row">
+          <div class="col-md-2">
+            <a class="navbar-brand" href="{{BASE}}">
+              <!-- <img src="img/logo.png" alt="My Receitas"> -->
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button> 
+          </div>
 
-  </body>
+          <div class="col-md-10">
+            <div class="collapse navbar-collapse" id="navbarColor01">
+              <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                  <a class="nav-link active" href="#">Home
+                    <span class="visually-hidden">(current)</span>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                  <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Separated link</a>
+                  </div>
+                </li>
+              </ul>
+              <form class="d-flex">
+                <input class="form-control me-sm-2" type="text" placeholder="Search">
+                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
+        </div> 
+      </div>
+    </nav>
+
+  <div class="max-width">
+
+    {% block body %} {% endblock %}
+
+  </div>
+
+</body>
 </html>
