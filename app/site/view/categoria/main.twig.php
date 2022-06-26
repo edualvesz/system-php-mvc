b700dc0eacfe93ec937b18fa7c4498759a8b23b2
edualvesz <ea-hendrix@hotmail.com>
2022-06-26 17:24:06 -0300

    calling main modal using javascript

--- app/site/view/categoria/main.twig.php
+++ app/site/view/categoria/main.twig.php
@@ -5,9 +5,17 @@
 {% block body %}
  <!-- the content here must be imported to the home/main.twig.php -->
 <h1>Categorias</h1>
-<a href="{{BASE}}categoria/adicionar/" class="btn btn-primary">Nova categoria</a>
+<!-- <a href="{{BASE}}categoria/adicionar/" class="btn btn-primary" onclick="salvaCategoria()">Nova categoria</a> -->
+<button type="submit" class="btn btn-primary" onclick="salvaCategoria()">Nova Categoria</button>
+
 
 <hr>
+<script> 
+  function salvaCategoria(){
+    $('#salvaCategoria').modal('show')
+    document.getElementById('id')
+  }
+</script>
 
 <div class="overflow-auto"></div>
   <table class="table table-hover">
@@ -19,5 +27,24 @@
       </tr>
     </thead>
   </table>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="salvaCategoria" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
 {% endblock %}
